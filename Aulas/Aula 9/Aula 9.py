5101c47308459e7057349173ab25039d51de508e
Vinicius Teles <vinitelessilva@hotmail.com>
2022-11-26 02:23:49 -0300

    Creating a new folder for exercises

---
+++ Aulas/Aula 9/Aula 9.py
@@ -0,0 +1,10 @@
+frase = 'Curso em Video Python'
+
+print('len', len(frase))
+print('count', frase.count('o'))
+print('find', frase.find('e'))
+print('replace', frase.replace('Python', 'Android'))
+print('lower', frase.lower())
+print('uuper', frase.upper())
+print('capitalize', frase.capitalize())
+print('title', frase.title())
